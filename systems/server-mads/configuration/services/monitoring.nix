b50624f45957aa560d95bb00e21d92999e5b17ea
mads256h <mail@madsmogensen.dk>
2026-08-20 18:57:41 +0200

    tmp fix for fail2ban exporter

--- systems/server-mads/configuration/services/monitoring.nix
+++ systems/server-mads/configuration/services/monitoring.nix
@@ -83,4 +83,12 @@
       extraConfig = "stub_status;";
     };
   };
+
+  # Fix for fail2ban exporter
+  systemd.services."prometheus-fail2ban-exporter".serviceConfig.ExecStart = lib.mkForce ''
+    ${lib.getExe pkgs.prometheus-fail2ban-exporter} \
+      ${lib.optionalString config.services.prometheus.exporters.fail2ban.exitOnError ''--collector.f2b.exit-on-socket-connection-error \''}
+      --web.listen-address="${config.services.prometheus.exporters.fail2ban.host}:${toString config.services.prometheus.exporters.fail2ban.port}" \
+      --collector.f2b.socket=${config.services.prometheus.exporters.fail2ban.fail2banSocket}
+  '';
 }
