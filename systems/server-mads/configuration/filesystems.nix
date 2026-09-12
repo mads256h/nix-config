7286be24ea9238d038987662af7a40210d8d07ac
mads256h <mail@madsmogensen.dk>
2026-09-12 16:56:00 +0200

    Set nginx charset to utf8

--- systems/server-mads/configuration/filesystems.nix
+++ systems/server-mads/configuration/filesystems.nix
@@ -90,7 +90,7 @@
 
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
-  
+
   boot.swraid.enable = true;
   boot.swraid.mdadmConf = "MAILADDR mads" + "256" + "h" + "@pro" + "tonm" + "ail" + ".com";
 }
