d2d1c39021e30caddcf77981c4494b6e5e50028e
mads256h <mail@madsmogensen.dk>
2026-09-17 08:32:53 +0200

    Use zen on interactive systems

--- systems/desktop-mads/configuration/default.nix
+++ systems/desktop-mads/configuration/default.nix
@@ -15,7 +15,7 @@
   };
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   networking.hostName = "desktop-mads"; # Define your hostname.
   # Pick only one of the below networking options.
