1d2ecfca96aa1ab60c0f8188e0860e2030888a5b
mads256h <mail@madsmogensen.dk>
2026-09-17 08:32:53 +0200

    Use zen on interactive systems

--- systems/laptop-mads/configuration/default.nix
+++ systems/laptop-mads/configuration/default.nix
@@ -16,7 +16,7 @@
   };
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.kernelParams = [
     "pcie_aspm=off"
     "pcie_port_pm=off"
